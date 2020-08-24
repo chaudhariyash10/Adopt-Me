3deba9ae23fc1124e6a377c5e500afb0716898d9
Yash Chaudhari <chaudhariyash10@gmail.com>
2020-08-24 20:05:30 +0530

    After Adding Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
-import SearchParams from './SearchParams'
+import React from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-     <SearchParams />
+
+      <Router>
+        <Route path="/" exact component={SearchParams} />
+        <Route path="/details/" component={Details} />
+      </Router>
     </div>
-  )
+  );
 };
 
-render(<App/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default function Pet ({ name, animal, breed, media, location, id }) {
 
   let hero = "http://placecorgi.com/300/300";
